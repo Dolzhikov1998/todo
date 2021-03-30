dfe520e07b4a50c579b6ebfdefe08f7e24ade360
Dolzhikov1998 <ilyadolzhkv@gmail.com>
2021-03-30 16:03:23 +0300

    add dotenv

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,35 +1,15 @@
 import axios from 'axios'
 
+const url = process.env.REACT_APP_API
 
-// const API_URL = 'https://todo-api-learning.herokuapp.com/'
-// const API_URL = 'http://localhost:3000/api/'
-// const URL = 'https://calm-earth-60546.herokuapp.com/api/'
-
-const {REACT_APP_URL} = process.env
-console.log("ASDASDAD"+REACT_APP_URL)
 const instance = axios.create(
     {
-        baseURL: REACT_APP_URL,
+        baseURL: url,
         headers: {
             'Content-Type': 'application/json',
         }
     }
 )
-
-
-// const request  = async (method, api, data) => {
-//     try{
-//         return await instance({
-//             method,
-//             url:api,
-//             data
-//         })
-//     }
-//     catch(e){
-//         console.log(e)
-//     }
-// }
-
 
 export const addTask = async (record) => {
     const response = await instance.post(`cards`, record)
